72879bdedbd3d9bc9f674f6332f1057c9f720787
sathwikmethari <sathwikmethari@gmail.com>
2025-08-07 09:55:11 +0530

    added functions

---
+++ src/utils.py
@@ -0,0 +1,47 @@
+import email, datetime, imapclient
+from typing import List
+from email import message_from_bytes
+from email.policy import default
+
+def get_ids(server : imapclient.imapclient.IMAPClient, from_date : datetime.date) -> List[int]:
+    
+   return server.search(criteria=[u'SINCE', from_date])
+
+
+def parse_email_address(addr: imapclient.response_types.Address):
+    if not addr:
+        return None, None
+    name = addr.name.decode() if addr.name else None
+    email_addr = f"{addr.mailbox.decode()}@{addr.host.decode()}" if addr.mailbox and addr.host else None
+    return name, email_addr
+
+
+def has_attachments(message : email.message.EmailMessage) -> bool:
+    return  any(message.iter_attachments())
+
+
+def get_msg_data(server : imapclient.imapclient.IMAPClient, list_of_ids: List[int]):
+    
+    #UPDATE THIS(LIST OF IDS) AFTER TESTING
+    for msgid, data in server.fetch(list_of_ids[-6], ['ENVELOPE', 'BODY[]']).items():
+        # Extract envelope info
+        sender = data[b'ENVELOPE'].sender[0]
+        name, email_addr = parse_email_address(sender)
+        
+        # Get raw email and parse body
+        msg = email.message_from_bytes(data[b'BODY[]'], policy=default)
+        # Extract body
+        if msg.is_multipart():
+            for part in msg.walk():
+                content_type = part.get_content_type()
+                content_disposition = str(part.get("Content-Disposition"))
+
+                if "attachment" not in content_disposition and content_type == "text/plain":
+                    body = part.get_payload(decode=True).decode()
+                    break
+        else:
+            # Non-multipart email
+            charset =msg.get_content_charset()
+            body = msg.get_payload(decode=True).decode(charset)
+        
+    return name, email_addr, body    
