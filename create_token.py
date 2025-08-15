21b40110b9850a37af237d5a2fa971af39ac2a9c
sathwikmethari <sathwikmethari@gmail.com>
2025-08-15 19:29:31 +0530

    async p2-2

--- create_token.py
+++ create_token.py
@@ -1,16 +1,11 @@
-import os.path
-
+import os
+from dotenv import load_dotenv
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 #https://developers.google.com/workspace/gmail/api/quickstart/python
-def main():
+def main(creds_path: str, token_path: str, SCOPES) -> None:
   """Shows basic usage of the Gmail API.
   Lists the user's Gmail labels.
   """
@@ -18,8 +13,6 @@ def main():
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
-  token_path = "include/secrets/gmail/token.json"
-  creds_path = "include/secrets/gmail/credentials.json"
   if os.path.exists(token_path):
     creds = Credentials.from_authorized_user_file(token_path, SCOPES)
   # If there are no (valid) credentials available, let the user log in.
@@ -34,24 +27,11 @@ def main():
     # Save the credentials for the next run
     with open(token_path, "w") as token:
       token.write(creds.to_json())
-
-  try:
-    # Call the Gmail API
-    service = build("gmail", "v1", credentials=creds)
-    results = service.users().labels().list(userId="me").execute()
-    labels = results.get("labels", [])
-
-    if not labels:
-      print("No labels found.")
-      return
-    print("Labels:")
-    for label in labels:
-      print(label["name"])
-
-  except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
-    print(f"An error occurred: {error}")
-
-
-if __name__ == "__main__":
-  main()
+      
+if __name__ == "__main__":    
+  # If modifying these scopes, delete the file token.json.
+  SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+  load_dotenv()
+  token_path = os.getenv("token_path")
+  creds_path = os.getenv("credentials_path")
+  main(creds_path, token_path, SCOPES)
