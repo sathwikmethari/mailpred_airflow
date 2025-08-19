71e1a69ba0c025e0399099977938f08f3a79eca6
sathwikmethari <sathwikmethari@gmail.com>
2025-08-19 19:25:43 +0530

    tokenize p2

--- get_starred.py
+++ get_starred.py
@@ -9,7 +9,9 @@ def get_starred_emails(token_path):
     SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']    
     creds = Credentials.from_authorized_user_file(token_path, SCOPES)
     service = build('gmail', 'v1', credentials=creds)
-    results = service.users().messages().list(userId='me', q="is:starred", maxResults=500).execute()
+    #query = "is:starred"
+    query = "label:unimportant"
+    results = service.users().messages().list(userId='me', q=query, maxResults=500).execute()
     ids = [dict_["id"]  for dict_ in results.get('messages', [])]
     print(f"Fetched >>>> {len(ids)} Ids")
     return ids
@@ -18,11 +20,11 @@ if __name__ == "__main__":
     load_dotenv()
     token_path = os.getenv("token_path")
     ids = get_starred_emails(token_path)
-    out_dict  = wrapper_for_payload(ids, token_path)
-    print("Starting encoding")
-    # Compressing
-    bytes = msgspec.msgpack.encode(out_dict)        
-    zip_path = f"starred-{datetime.now().strftime('%d-%m-%Y-%H-%M')}.json.gz"
-    with gzip.open(zip_path, 'wb') as f:
-        f.write(bytes)
+    # out_dict  = wrapper_for_payload(ids, token_path)
+    # print("Starting encoding")
+    # # Compressing
+    # bytes = msgspec.msgpack.encode(out_dict)        
+    # zip_path = f"starred-{datetime.now().strftime('%d-%m-%Y-%H-%M')}.json.gz"
+    # with gzip.open(zip_path, 'wb') as f:
+    #     f.write(bytes)
 
